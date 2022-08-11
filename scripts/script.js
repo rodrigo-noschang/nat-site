dc26f7c16ae2996eb6c1422b0aff0615d58484b1
Rodrigo Noschang <rodrigo@hotmail.com>
2022-08-11 13:24:56 -0300

    moving header logo to the right a bit, and status update when the page is opened

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+const isOfficeOpen = () => {
+    const localHour = new Date().getUTCHours() - 3;
+    return localHour >= 9 && localHour < 18;    
+}
+
+const updateOfficeStatus = () => {
+    const statusElement = document.querySelector('.keep-up-facebook-status')
+
+
+    if (isOfficeOpen()) {
+        statusElement.innerText = 'Aberto agora';
+        statusElement.classList.add('keep-up-facebook-status-open');
+    } else {
+        statusElement.innerText = 'Fechado';
+        statusElement.classList.remove('keep-up-facebook-status-open');
+    }
+}
+
+updateOfficeStatus();
