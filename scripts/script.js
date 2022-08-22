379c284ca7c506659323df4651ff2ab368231d55
Rodrigo Noschang <rodrigo@hotmail.com>
2022-08-22 18:39:18 -0300

    drop down on acting areas part

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,28 @@ const updateOfficeStatus = () => {
 }
 
 updateOfficeStatus();
+
+/* ========= Open/Close Acting Areas ========== */
+const areasItems = document.querySelectorAll('.area-item');
+const areasDescription = document.querySelectorAll('.area-description');
+const areas = document.querySelectorAll('.area-name');
+const areasArrows = document.querySelectorAll('.area-name-arrow');
+
+const addArrowsEventListeners = () => {
+    for (let i = 0; i < areasItems.length; i++) {
+        areas[i].addEventListener('click', () => {
+            areasItems[i].classList.toggle('shown-item');
+            areasDescription[i].classList.toggle('shown-description');
+            areasArrows[i].classList.toggle('shown-arrow');
+            
+        })
+    }
+    
+}
+
+
+addArrowsEventListeners();
+/* for (let i = 0; i < areasItems.length; i++) {
+    console.log(areasItems[i], areasDescription[i], arrows[i]);
+} */
+
